174879306b3d1aa79dfdd59ac04a8e3e007e029a
Belfor <alemandeleon86@gmail.com>
2020-03-21 12:31:31 +0000

    Pelota chunga

---
+++ Stupid League/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 0.1F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x + velocidad, 0, 0);   
+    }
+}
